90404faefbab1ed1fb1fe67e967c759962f0f646
wayne <wayne.gong.cn@hotmail.com>
2022-01-23 14:51:45 +0800

    ci: 增加了上传 ChromeStore 脚本

--- src/themes/default/index.tsx
+++ src/themes/default/index.tsx
@@ -6,7 +6,7 @@ import Login from '../../components/Login';
 import OpenMSTodo from '../../components/OpenMSToDo';
 import { backgroundContext } from '../../helpers/background';
 import { now, timing } from '../../helpers/report';
-import { Container, Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
 const Default: React.FC<void> = () => {
   useEffect(() => {
